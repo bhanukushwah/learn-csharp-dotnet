32f0a939ac70e6b8bb8b17755ade7fa212546a63
bhanukushwah <bhanupskofficial@gmail.com>
2022-07-14 13:38:09 +0530

    Added LengthHelper class

---
+++ LengthHelper/ClassLibrary1/Class1.cs
@@ -0,0 +1,18 @@
+﻿namespace LengthHelper
+{
+    public class LengthHelper
+    {
+        public static void Main()
+        {
+            LengthHelper lengthHelper = new LengthHelper();
+            Console.WriteLine("Please enter somehing to get length of it: ");
+            string input = Console.ReadLine();
+            int length = lengthHelper.GetLength(input);
+            Console.WriteLine(length);
+        }
+        public int GetLength(string text)
+        {
+            return text.Length;
+        } 
+    }
+}
