262319e2a79dfdb5086155c05b5cf250b630c163
bhanukushwah <bhanupskofficial@gmail.com>
2022-07-19 00:02:14 +0530

    Added counter task and simple banking application

---
+++ SingletonCounter/SingletonCounter/Counter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SingletonCounter
+{
+    public class Counter
+    {
+        private Counter() { }
+
+        private static Counter? _instance;
+        public int count = 0;
+        public static Counter GetCounter()
+        {
+            if (_instance == null)
+            {
+                _instance = new Counter();
+            }
+
+            return _instance;
+        }
+
+        public void increment()
+        {
+            count++;
+        }
+        public void decrement()
+        {
+            count--;
+        }
+    }
+}
---
+++ SingletonCounter/SingletonCounter/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SingletonCounter;
+public class Program
+{
+   public static void Main(string[] args)
+    {
+        Counter counter = Counter.GetCounter();
+        Counter counter2 = Counter.GetCounter();
+        Counter counter3 = Counter.GetCounter();
+
+        counter.increment();
+        counter.increment();
+        counter.increment();
+
+        counter2.increment();
+        counter2.increment();
+
+        counter3.increment();
+
+        Console.WriteLine("counter: " + counter.count);
+        Console.WriteLine("counter2: " + counter2.count);
+        Console.WriteLine("counter3: " + counter3.count);
+
+        if (counter == counter2 && counter2 == counter3)
+        {
+            Console.WriteLine("Singleton works, as all variables contain the same instance.");
+        }
+        else
+        {
+            Console.WriteLine("Singleton failed, as all contain different instances.");
+        }
+    }
+}
+
